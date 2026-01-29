01969aa8049b1b548d774496a9b872c344db526b
gerardalessandrov <gerardvigorodrigues@gmail.com>
2026-01-29 16:54:26 -0500

    next

--- resume-generator/next.config.ts
+++ resume-generator/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // Agrega esto para Vercel
+  reactStrictMode: true,
 };
 
 export default nextConfig;
